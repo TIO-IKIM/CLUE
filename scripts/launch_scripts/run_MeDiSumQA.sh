3a6737ad7122ce64d4d93eac5398be7cc84e33cf
dadaamin <amin.dada@rub.de>
2024-05-06 01:14:10 +0200

    update launch scripts

--- scripts/launch_scripts/run_MeDiSumQA.sh
+++ scripts/launch_scripts/run_MeDiSumQA.sh
@@ -1,14 +1,38 @@
+#!/bin/bash
+
+# Tasks specific settings
 eval_script="eval/eval_MeDiSumQA.py"
 dataset="data/MeDiSumQA/MeDiSumQA.json"
-log_path="predictions/MeDiSumQA"
-model_address=$1
-model_id=$2
-model_name="$(basename $model_id)"
+prediction_path="predictions/MeDiSumQA"
+num_few_shot_examples=1
 
-python $eval_script \
-    --model_address $1 \
-    --model_name_or_path $2 \
-    --num_few_shot_examples 1 \
-    --model_is_instruct \
-    --data_path $dataset \
-    --log_path $log_path/model_name
+# Default values for flags
+model_has_system=false
+model_is_instruct=false
+
+# Extract parameters using named flags
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        --model_address) model_address="$2"; shift ;;
+        --model_id) model_id="$2"; shift ;;
+        --model_has_system) model_has_system=true ;;
+        --model_is_instruct) model_is_instruct=true ;;
+        --log_path) log_path="$2"; shift ;;
+        --token) token="$2"; shift ;;
+        *) echo "Unknown parameter: $1" ;;
+    esac
+    shift
+done
+
+# Paths and script specific parameters
+[[ -z "$log_path" ]] && log_path="$prediction_path/$(basename "$model_id")"
+
+# Command construction
+cmd="python $eval_script --model_address $model_address --model_name_or_path $model_id"
+[[ $model_has_system == true ]] && cmd+=" --model_has_system"
+[[ $model_is_instruct == true ]] && cmd+=" --model_is_instruct"
+cmd+=" --num_few_shot_examples $num_few_shot_examples --data_path $dataset --log_path $log_path"
+[[ -n $token ]] && cmd+=" --hf_token $token"
+
+# Execute the command
+echo $cmd
